d957116df4dbb8c40a2856a7a452e43ddbc7f710
Petar <petarburic554@gmail.com>
2024-02-23 23:55:12 +0100

    Add user login feature with API integration

--- logic.js
+++ logic.js
@@ -2,17 +2,17 @@ let apiMainUrl = 'https://x8ki-letl-twmt.n7.xano.io/api:MYXwu2O1';
 
 // user registraion
 if (document.getElementById('registerBtn')) {
-    document.getElementById('registerBtn').onclick = function(e) {
+    document.getElementById('registerBtn').onclick = function (e) {
         e.preventDefault();
-    
+
         let name = document.getElementById('name').value;
         let email = document.getElementById('email').value;
         let password = document.getElementById('password').value;
         let phone = document.getElementById('phone').value;
         let repeat_password = document.getElementById('repeat_password').value;
-    
+
         let apiEndpoint = apiMainUrl + "/auth/signup";
-    
+
         let requestBody = {
             name,
             email,
@@ -20,7 +20,7 @@ if (document.getElementById('registerBtn')) {
             phone,
             repeat_password
         }
-    
+
         fetch(apiEndpoint, {
             method: 'POST',
             headers: {
@@ -28,30 +28,30 @@ if (document.getElementById('registerBtn')) {
             },
             body: JSON.stringify(requestBody)
         })
-        .then(response => {
-            if (!response.ok) {
-                return response.json().then(errorData => {
-                    console.error("Error data:", errorData);
-                    throw new Error('Server responded with a non-OK status');
-                });
-            }
-            return response.json();
-        })
-        .then(data => {
-            if(data.authToken) {
-                localStorage.setItem('authToken', data.authToken);
-                window.location.href = 'novi_oglas.html';
-            }
-        })
-        .catch(error => {
-            console.error("Request failed:", error);
-        });
+            .then(response => {
+                if (!response.ok) {
+                    return response.json().then(errorData => {
+                        console.error("Error data:", errorData);
+                        throw new Error('Server responded with a non-OK status');
+                    });
+                }
+                return response.json();
+            })
+            .then(data => {
+                if (data.authToken) {
+                    localStorage.setItem('authToken', data.authToken);
+                    window.location.href = 'novi_oglas.html';
+                }
+            })
+            .catch(error => {
+                console.error("Request failed:", error);
+            });
     }
 }
 
 // adding ads
 if (document.getElementById('noviOglasBtn')) {
-    document.getElementById('noviOglasBtn').onclick = function(e) {
+    document.getElementById('noviOglasBtn').onclick = function (e) {
         e.preventDefault();
 
         let marka = document.getElementById('marka').value;
@@ -73,7 +73,7 @@ if (document.getElementById('noviOglasBtn')) {
         formData.append('file', file.files[0]);
 
         let authToken = localStorage.getItem('authToken');
-    
+
         fetch(apiEndpoint, {
             method: 'POST',
             headers: {
@@ -81,30 +81,84 @@ if (document.getElementById('noviOglasBtn')) {
             },
             body: formData
         })
-        .then(response => {
-            if (!response.ok) {
-                return response.json().then(errorData => {
-                    console.error("Error data:", errorData);
-                    throw new Error('Server responded with a non-OK status.');
-                });
-            }
-            return response.json();
+            .then(response => {
+                if (!response.ok) {
+                    return response.json().then(errorData => {
+                        console.error("Error data:", errorData);
+                        throw new Error('Server responded with a non-OK status.');
+                    });
+                }
+                return response.json();
+            })
+            .then(data => {
+                alert('Ad successfully added. Awaiting administrator approval.');
+                //location.reload();
+            })
+            .catch(error => {
+                console.error("Request failed:", error);
+            });
+    }
+}
+
+// user login
+if (document.getElementById('loginBtn')) {
+    document.getElementById('loginBtn').onclick = function (e) {
+        e.preventDefault();
+
+        let email = document.getElementById('email').value;
+        let password = document.getElementById('password').value;
+
+        let requestBody = {
+            email,
+            password
+        }
+
+        let apiEndpoint = apiMainUrl + "/auth/login";
+
+        fetch(apiEndpoint, {
+            method: 'POST',
+            headers: {
+                'Content-type': 'application/json'
+            },
+            body: JSON.stringify(requestBody)
         })
-        .then(data => {
-            alert('Ad successfully added. Awaiting administrator approval.');
-            //location.reload();
-        })
-        .catch(error => {
-            console.error("Request failed:", error);
-        });
+            .then(response => {
+                if (!response.ok) {
+                    return response.json().then(errorData => {
+                        console.error("Error data:", errorData);
+                        throw new Error('Server responded with a non-OK status.');
+                    });
+                }
+                return response.json();
+            })
+            .then(data => {
+                if (data.authToken) {
+                    localStorage.setItem('authToken', data.authToken);
+                    window.location.href = 'novi_oglas.html';
+                }
+            })
+            .catch(error => {
+                console.error("Request failed:", error);
+            });
     }
 }
 
 // user logout
 if (document.getElementById('odjaviSe')) {
-    document.getElementById('odjaviSe').onclick = function(e) {
+    document.getElementById('odjaviSe').onclick = function (e) {
         e.preventDefault();
 
+        localStorage.clear();
+        window.location.href = 'index.html';
+    }
+}
+
+if (localStorage.getItem('authToken')) {
+    document.getElementById("navigation").innerHTML = `<a href="novi_oglas.html" class="btn btn-warning">Add ad</a>
+                                                       <a href="#" id="odjaviSe" class="btn btn-info">Log out</a>`
+
+    document.getElementById('odjaviSe').onclick = function (e) {
+        e.preventDefault();
         localStorage.clear();
         window.location.href = 'index.html';
     }
