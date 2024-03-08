9089c3b69ba0d23966fc095297e6fe84236a4feb
Petar.Buric <petarburic554@gmail.com>
2024-03-08 22:24:08 +0100

    Code refactoring - second part

--- logic.js
+++ logic.js
@@ -2,14 +2,18 @@ let apiMainUrl = 'https://x8ki-letl-twmt.n7.xano.io/api:MYXwu2O1';
 let loader = document.querySelector('.loader-overlay'); 
 
 // function for POST request
-function sendPostRequest(apiEndpoint, requestBody, successCallback) {
-    fetch(apiEndpoint, {
+function sendPostRequest(apiEndpoint, requestBody, successCallback, headers = {'Content-type': 'application/json'}, isFormData = false) {
+    const body = isFormData ? requestBody : JSON.stringify(requestBody);
+
+    const fetchOptions = {
         method: 'POST',
-        headers: {
-            'Content-type': 'application/json'
-        },
-        body: JSON.stringify(requestBody)
-    })
+        headers: headers,
+        body: body
+    };
+
+    loader.style.display = 'flex';
+
+    fetch(apiEndpoint, fetchOptions)
     .then(response => {
         if (!response.ok) {
             return response.json().then(errorData => {
@@ -22,6 +26,25 @@ function sendPostRequest(apiEndpoint, requestBody, successCallback) {
     .then(data => successCallback(data))
     .catch(error => console.error("Request failed:", error))
     .finally(() => loader.style.display = 'none');
+}
+
+// function for GET request
+function sendGetRequest(apiEndpoint, successCallback) {
+    loader.style.display = 'flex';
+
+    fetch(apiEndpoint)
+        .then(response => {
+            if (!response.ok) {
+                return response.json().then(errorData => {
+                    console.error("Error data:", errorData);
+                    throw new Error('Server responded with a non-OK status.');
+                });
+            }
+            return response.json();
+        })
+        .then(data => successCallback(data))
+        .catch(error => console.error("Request failed:", error))
+        .finally(() => loader.style.display = 'none');
 }
 
 // user registraion
@@ -37,7 +60,6 @@ function handleRegistration(e) {
     let requestBody = { name, email, password, phone, repeat_password };
     let apiEndpoint = `${apiMainUrl}/auth/signup`;
 
-    loader.style.display = 'flex';
     sendPostRequest(apiEndpoint, requestBody, data => {
         if (data.authToken) {
             localStorage.setItem('authToken', data.authToken);
@@ -63,9 +85,7 @@ function handleUserLogin(e) {
     }
 
     let apiEndpoint = apiMainUrl + "/auth/login";
-    loader.style.display = 'flex';
 
-    loader.style.display = 'flex';
     sendPostRequest(apiEndpoint, requestBody, data => {
         if (data.authToken) {
             localStorage.setItem('authToken', data.authToken);
@@ -92,6 +112,7 @@ if (document.getElementById('odjaviSe')) {
 if (localStorage.getItem('authToken')) {
     document.getElementById("navigation").innerHTML = `<a href="novi_oglas.html" class="btn btn-warning">Add ad</a>
                                                        <a href="#" id="odjaviSe" class="btn btn-info">Log out</a>`
+
     document.getElementById('odjaviSe').onclick = function (e) {
         e.preventDefault();
         localStorage.clear();
@@ -99,128 +120,75 @@ if (localStorage.getItem('authToken')) {
     }
 }  
 
+// handling add ads
+function addingAds(e) {
+    e.preventDefault();
 
-// adding ads
+    let marka = document.getElementById('marka').value;
+    let price = document.getElementById('price').value;
+    let fuel = document.getElementById('fuel').value;
+    let year = document.getElementById('year').value;
+    let karoserija = document.getElementById('karoserija').value;
+    let file = document.getElementById('file');
+
+    let apiEndpoint = apiMainUrl + "/car";
+
+    let formData = new FormData();
+
+    formData.append('marka', marka);
+    formData.append('price', price);
+    formData.append('fuel', fuel);
+    formData.append('year', year);
+    formData.append('karoserija', karoserija);
+    formData.append('file', file.files[0]);
+
+    let authToken = localStorage.getItem('authToken');
+    let headers = {
+        'Authorization': 'Bearer ' + authToken
+    };
+
+    sendPostRequest(apiEndpoint, formData, data => {
+        alert('Ad successfully added. Awaiting administrator approval.');
+        location.reload();
+    }, headers, true);
+}
+
 if (document.getElementById('noviOglasBtn')) {
-    document.getElementById('noviOglasBtn').onclick = function (e) {
-        e.preventDefault();
-
-        let marka = document.getElementById('marka').value;
-        let price = document.getElementById('price').value;
-        let fuel = document.getElementById('fuel').value;
-        let year = document.getElementById('year').value;
-        let karoserija = document.getElementById('karoserija').value;
-        let file = document.getElementById('file');
-
-        let apiEndpoint = apiMainUrl + "/car";
-
-        let formData = new FormData();
-
-        formData.append('marka', marka);
-        formData.append('price', price);
-        formData.append('fuel', fuel);
-        formData.append('year', year);
-        formData.append('karoserija', karoserija);
-        formData.append('file', file.files[0]);
-
-        let authToken = localStorage.getItem('authToken');
-        loader.style.display = 'flex';
-
-        fetch(apiEndpoint, {
-            method: 'POST',
-            headers: {
-                'Authorization': 'Bearer ' + authToken
-            },
-            body: formData
-        })
-            .then(response => {
-                if (!response.ok) {
-                    return response.json().then(errorData => {
-                        console.error("Error data:", errorData);
-                        throw new Error('Server responded with a non-OK status.');
-                    });
-                }
-                return response.json();
-            })
-            .then(data => {
-                alert('Ad successfully added. Awaiting administrator approval.');
-                loader.style.display = 'none';
-                location.reload();
-            })
-            .catch(error => {
-                console.error("Request failed:", error);
-            });
-    }
+    document.getElementById('noviOglasBtn').onclick = addingAds;
 }
 
-// all ads
+// handling of getting all ads
 if (document.getElementById('pretraziBtn')) {
-    document.getElementById('pretraziBtn').onclick = function (e) {
-        // e.preventDefault();
-
+    document.getElementById('pretraziBtn').onclick = function () {
         let apiEndpoint = apiMainUrl + "/car";
-        loader.style.display = 'flex';
-
-        fetch(apiEndpoint)
-            .then(response => {
-                if (!response.ok) {
-                    return response.json().then(errorData => {
-                        console.error("Error data:", errorData);
-                        throw new Error('Server responded with a non-OK status.');
-                    });
-                }
-                return response.json();
-            })
-            .then(cars => {
-                loader.style.display = 'none';
-
-                let container = document.getElementById('sviOglasi');
-
-                cars.forEach(car => {
-                    let carElement = document.createElement('div');
-
-                    carElement.className = 'col-sm-4';
-                    carElement.innerHTML = `
-                        <div class="car-item-wrapper">
-                            <img src="${car.car_image.url}?tpl=big" alt="${car.marka}">
-                            <h4>${car.marka}</h4>
-                            <p>Cijena: ${car.price}</p>
-                            <p>Godište: ${car.year}</p>
-                            <a class"btn btn-warning" href="car.html?id=${car.id}">Vidi više</a>
-                        <div/>
-                        `
-                    container.appendChild(carElement);
-                });
-            })
-            .catch(error => {
-                console.error("Request failed:", error);
+        sendGetRequest(apiEndpoint, function(cars) {
+            let container = document.getElementById('sviOglasi');
+            cars.forEach(car => {
+                let carElement = document.createElement('div');
+                carElement.className = 'col-sm-4';
+                carElement.innerHTML = `
+                    <div class="car-item-wrapper">
+                        <img src="${car.car_image.url}?tpl=big" alt="${car.marka}">
+                        <h4>${car.marka}</h4>
+                        <p>Cijena: ${car.price}</p>
+                        <p>Godište: ${car.year}</p>
+                        <a class="btn btn-warning" href="car.html?id=${car.id}">Vidi više</a>
+                    </div>`;
+                container.appendChild(carElement);
             });
+        });
     }
 }
 
-// selected ad
+// getting the selected ad
 if (document.getElementById('appendImage')) {
     let urlParams = new URLSearchParams(window.location.search);
     let car_id = urlParams.get('id');
     let apiEndpoint = apiMainUrl + "/car/" + car_id;
 
-    loader.style.display = 'flex';
-
-    fetch(apiEndpoint)
-        .then(response => {
-            if (!response.ok) {
-                return response.json().then(errorData => {
-                    console.error("Error data:", errorData);
-                    throw new Error('Server responded with a non-OK status.');
-                });
-            }
-            return response.json();
-        })
-        .then(car => {
-            loader.style.display = 'none';
-            car = car[0];
-
-            let imageContainer = document.querySelector('#appendImage');
+    sendGetRequest(apiEndpoint, function(cars) {
+        let car = cars[0];
+        let imageContainer = document.querySelector('#appendImage');
             if (imageContainer) {
                 let img = document.createElement('img');
                 img.src = `${car.car_image.url}`;
@@ -242,66 +210,50 @@ if (document.getElementById('appendImage')) {
             } else {
                 alert('Content container #appendContent not found');
             }
-        })
-        .catch(error => {
-            console.error("Request failed:", error);
-        });
+    });
 }
 
-//
-if(document.getElementById("pretraziBtn")) {
-    let currentUrl = window.location.href;
+// search functionality
+if(document.getElementById("pretraziBtn") && window.location.href.includes('?')) {
+    let queryParams = new URLSearchParams(window.location.search);
 
-    if(currentUrl.includes('?')) {
-        let queryParams = new URLSearchParams(window.location.search);
+    let marka = queryParams.get('marka');
+    let year_from = queryParams.get('year_from');
+    let year_to = queryParams.get('year_to');
+    let price = queryParams.get('price');
+    let gorivo = queryParams.get('gorivo');
+    let karoserija = queryParams.get('karoserija');
 
-        let marka = queryParams.get('marka');
-        let year_from = queryParams.get('year_from');
-        let year_to = queryParams.get('year_to');
-        let price = queryParams.get('price');
-        let gorivo = queryParams.get('gorivo');
-        let karoserija = queryParams.get('karoserija');
+    if(marka) {
+        document.getElementById('marka').value = marka;
+    }
 
-        if(marka) {
-            document.getElementById('marka').value = marka;
-        }
+    if(year_from) {
+        document.getElementById('year_from').value = year_from;
+    }
 
-        if(year_from) {
-            document.getElementById('year_from').value = year_from;
-        }
+    if(year_to) {
+        document.getElementById('year_to').value = year_to;
+    }
 
-        if(year_to) {
-            document.getElementById('year_to').value = year_to;
-        }
+    if(price) {
+        document.getElementById('price').value = price;
+    }
 
-        if(price) {
-            document.getElementById('price').value = price;
-        }
+    if(gorivo) {
+        document.getElementById('gorivo').value = gorivo;
+    }
 
-        if(gorivo) {
-            document.getElementById('gorivo').value = gorivo;
-        }
+    if (karoserija) {
+        document.getElementById('karoserija').value = karoserija;
+    }
 
-        if(karoserija) {
-            document.getElementById('karoserija').value = karoserija;
-        }
+    let apiEndpoint = apiMainUrl + '/search'
+    apiEndpoint += `?marka=${encodeURIComponent(marka)}&year_from=${year_from}&year_to=${year_to}&price=${price}&gorivo=${encodeURIComponent(gorivo)}`
 
-        let apiEndpoint = apiMainUrl + '/search'
-        apiEndpoint += `?marka=${encodeURIComponent(marka)}&year_from=${year_from}&year_to=${year_to}&price=${price}&gorivo=${encodeURIComponent(gorivo)}`
-
-        loader.style.display = 'flex';
-
-        fetch(apiEndpoint, {
-            method: 'GET',
-            headers: {
-              'Content-Type': 'application/json'
-            }
-          })
-          .then(response => response.json())
-          .then(cars => {
-            loader.style.display = 'none';
-            let container = document.querySelector('#sviOglasi');
-            container.innerHTML = '';
+    sendGetRequest(apiEndpoint, function(cars) {
+        let container = document.querySelector('#sviOglasi');
+        container.innerHTML = '';
 
             console.log(cars);
           
@@ -318,9 +270,5 @@ if(document.getElementById("pretraziBtn")) {
                 </div>`;
               container.appendChild(carElement);
             });
-          
-            //loader.style.display = 'none';
-          });
-          
-    }
+    });
 }
